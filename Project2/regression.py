7085d81ae2966ad944f4680fbb81e9c8726849ae
Jake <jacobsolsky@gmail.com>
2023-10-24 16:43:11 +0200

    Add data standardisation

--- Project2/regression.py
+++ Project2/regression.py
@@ -9,4 +9,22 @@ import array_to_latex as a2l
 import seaborn as sns
 import math
 
-df = pd.read_csv("prostate.csv")
+df = pd.read_csv("Prostate_Cancer.csv", index_col="id")
+df["diagnosis_result"] = np.where(df["diagnosis_result"] == "M", 1, 0)
+
+continuous_cols = ['radius', 
+                    'texture', 
+                    'perimeter', 
+                    'area', 
+                    'smoothness', 
+                    'compactness', 
+                    'symmetry', 
+                    'fractal_dimension']
+                    
+df_means = df[continuous_cols].mean()
+
+df_std = df[continuous_cols].std()
+
+df[continuous_cols] = (df[continuous_cols] -  df_means) / df_std
+
+print(df)
